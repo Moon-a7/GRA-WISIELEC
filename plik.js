783a949ee1df64c1037b44abcf215d13f6e75059
username <monika.teslak@gmail.com>
2021-10-13 17:34:38 +0200

    correct winow.onload function.js

--- plik.js
+++ plik.js
@@ -4,15 +4,15 @@ window.onload = function () {
     'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-    const categories
-    const chosenCategory
-    const getHint
-    const word
-    const guess
-    const geusses = []
-    let lives
-    let counter
-    const space
+    const categories;
+    const chosenCategory;
+    //const getHint; // UZYSKAC WSKAZÓWKE
+    const word;
+    //const guess;// ODGADNĄĆ, ZAGADKA (?)
+    const geusses = []; // ODGADNIĘTE
+    let lives 
+    let counter //  LICZNIK
+    const space // PRZESTRZEŃ
 
     let showLives = document.getElementById("myLives")
     const showCategory = document.getElementById("scategory")
@@ -21,20 +21,20 @@ window.onload = function () {
 
     const buttons = function () {
         myButtons = document.getElementById('buttons')
-        letters = document.createElement('ul')
+        letters = document.createElement('ul') //LISTY
 
         for (var i = 0; i< alphabet.length; i++) {
             letters.id = 'alphabet';
             list = document.createElement('li');
             list.id = 'letter';
             list.innerHTML = alphabet[i];
-      check();
+      check(); //SPRAWDZAC 
       myButtons.appendChild(letters);
       letters.appendChild(list);
     }
 
         }
-}
+
 // SELECT CATEGORY
 const selectCat = function () {
     if (chosenCategory === categories [0] {
@@ -46,3 +46,34 @@ const selectCat = function () {
         categoryName.innerHTML = "The Chosen Category Is Cities"
     }
 }
+// CREATE GEUSSES ul 
+result = function () {
+    wordHolder = document.getElementById('hold')//
+    correct = document.createElement('ul')
+
+    for (let i = 0; i< word.length; i++) {
+        correct.setAttribute('id', 'my-word')
+        guess = document.createElement('li')
+        guess.setAttribute('class', 'guess')
+        if (word[i] === "-") {
+            guess.innerHTML = "-"
+            space = 1
+        } else {
+            guess.innerHTML = "_"
+        }
+        geusses.push(guess)
+        wordHolder.appendChild(correct)
+        correct.appendChild(guess)
+    }
+}
+// SHOW LIVES - pokaż życia
+comments = function () {
+showLives.innerHTML = "You have" + lives + "lives"
+if (lives < 1) {
+
+}
+}
+
+}
+
+    
